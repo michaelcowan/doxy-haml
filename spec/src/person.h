f668acd4ae0edd58c9ab8379c6c71fc6d96c4a58
Mike Cowan <cowan@adobe.com>
2015-09-14 13:53:22 +0100

    Adding person.h test class

---
+++ spec/src/person.h
@@ -0,0 +1,26 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+/**
+ *  \brief Represents a Person.
+ *
+ *  The class Person contains information and functions related to all people
+ *
+ *  \author Michael Cowan
+ */
+class Person {
+
+public:
+
+  /**
+   *  \brief Sets name.
+   *
+   *  Sets the name of the Person.
+   *
+   *  \param [in] name The name of the Person
+   */
+  void setName(const char * name);
+
+};
+
+#endif // PERSON_H
