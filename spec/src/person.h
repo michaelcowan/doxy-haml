41e1531de909c827427a20fc23b598e437665a38
Mike Cowan <mike.c.cowan@gmail.com>
2015-11-02 11:05:12 +0000

    Updating spec sample source to fix tests

--- spec/src/person.h
+++ spec/src/person.h
@@ -29,6 +29,16 @@ public:
    */
   void setName(const char * name);
 
+  /**
+   *  \brief Sets name.
+   *
+   *  Sets both names of the Person.
+   *
+   *  \param [in] firstname The first name of the Person
+   *  \param [in] secondname The second name of the Person
+   */
+  void setName(const char * firstname, const char * secondname);
+
 };
 
 #endif // PERSON_H
