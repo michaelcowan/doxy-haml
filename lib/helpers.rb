5383a22dee3156fca42e09092b38034babd4eb53
Mike Cowan <mike.c.cowan@gmail.com>
2015-11-26 11:13:15 +0000

    Moving doxygen_markup from html_utilities to helpers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -61,6 +61,32 @@ module DoxyHaml
     def sort_by_name objects
       objects.sort_by { |o| [alpha_name(o.name), o.name[0]] }
     end
+
+    def doxygen_markup doc, to_html = true
+      doc.css('programlisting').each { |n| n.name = 'pre'; n['class'] = 'src' }
+      doc.css('codeline').each { |n| n.name = 'span'; n['class'] = 'src-line' }
+      doc.css('highlight').each { |n| n.name = 'span'; n['class'] = "src-#{n['class']}" }
+      doc.css('sp').each { |n| n.replace(" ") }
+      # docMarkupType
+      doc.css('bold').each { |n| n.name = 'strong' }
+      doc.css('emphasis').each { |n| n.name = 'em' }
+      doc.css('computeroutput').each { |n| n.name = 'pre' }
+      doc.css('subscript').each { |n| n.name = 'sub' }
+      doc.css('superscript').each { |n| n.name = 'sup' }
+      doc.css('center').each { |n| n.name = 'center' }
+      doc.css('small').each { |n| n.name = 'small' }
+      # docURLLink
+      doc.css('ulink').each { |n| n.name = 'a'; n.attribute('url').name = 'href' }
+      # docRefTextType
+      doc.css('ref').each do |n|
+        n.name = 'a'
+        n.attribute('refid').name = 'href'
+        n['href'] = id_to_href n['href']
+        n.attribute('kindref').remove
+      end
+      # Generate text/HTML, remove extra spaces and substitute " for '
+      (to_html ? doc.inner_html : doc.inner_text).rstrip.gsub(/"/, "'")
+    end
     
   end
 
